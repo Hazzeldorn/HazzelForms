14c5adb00376dbd65be755b0415619a998efc23e
hazzeldorn <jonas@ohodesign.ch>
2020-07-21 18:52:27 +0200

    Lazy URL validation added

--- src/HazzelForms/Field/Text/Url.php
+++ src/HazzelForms/Field/Text/Url.php
@@ -4,17 +4,32 @@ namespace HazzelForms;
 
 class Url extends Text {
 
+    private const URL_REGEX = "@(http://|https://)?[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}(\/\S*)?@"; // lazy URL validation
+
+
     public function __construct($fieldName, $formName, $args = array())  {
         parent::__construct($fieldName, $formName, $args);
+
+        $this->strict = $args['strict'] ?? true;
 
         $this->fieldType = 'url';
     }
 
+
     public function validate() {
         if (parent::validate()) {
-            if (!empty($this->fieldValue) && !filter_var($this->fieldValue, FILTER_VALIDATE_URL)) {
-                $this->error = 'invalid';
+
+            if($this->strict){
+                // strict validation requires protocol prefix (http:// or https://)
+                if (!empty($this->fieldValue) && !filter_var($this->fieldValue, FILTER_VALIDATE_URL)) {
+                    $this->error = 'invalid';
+                }
+            } else {
+                if (!empty($this->fieldValue) && !preg_match(self::URL_REGEX, $this->fieldValue)) {
+                    $this->error = 'invalid';
+                }
             }
+
         }
         $this->validated = true;
         return $this->isValid();
